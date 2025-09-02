d99938d43560b64b0b31c63af2d1bed6b51a6343
Animesh Anand <animeshanand86033@gmail.com>
2025-09-02 13:44:14 +0530

    FirstOne

---
+++ README.md
@@ -0,0 +1,6 @@
+# 📘 React Learning Repository
+
+This repository contains my React learning projects, practice code, and experiments.  
+I use this repo to track my progress while learning React concepts.
+
+---
---
+++ Rivise.js
@@ -0,0 +1,7 @@
+
+
+        const heading = React.createElement("div", {}, React.createElement("h2", {}, "Ish Text ko HEADING me Daal do!"));
+
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(heading);
